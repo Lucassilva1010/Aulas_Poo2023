b4571b4dc3b17c4376b5e6e163a04358b7ecf416
Lucas Augusto <lucas_lapada@hotmail.com>
2023-03-21 20:53:00 -0300

    Sessão 05 concluida Classes, Atributos, metodos, menbros estaticos

---
+++ Exercicios/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Retangulo dados = new Retangulo();
+
+            Console.Write("Digite a Largura: ");
+            dados.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite a Altura: ");
+            dados.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            dados.Area();
+            dados.Perimetro();
+            dados.Diagonal();
+
+            Console.WriteLine(dados);
+        }
+    }
+}
---
+++ Exercicios/Retangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    public class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+           return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura+Altura)*2 ;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Largura*2)+(Largura*2)) ;
+        }
+
+        public override string ToString()
+        {
+            return "A area é: "+Area().ToString("f2",CultureInfo.InvariantCulture)+"\n"
+                +"O perimetro é: "+Perimetro().ToString("f2",CultureInfo.InvariantCulture)+"\n"
+                +"A diaginal do Quadrado é: "+Diagonal().ToString("f2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
